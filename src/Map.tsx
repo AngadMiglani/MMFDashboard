287b830dc40c37141acb70a5d4da8e5ab42e8012
AngadMiglani <82772584+AngadMiglani@users.noreply.github.com>
2024-06-04 23:43:19 +0530

    Fixing the white screen

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,31 +1,28 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import React, { useState } from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
-
 const Map = ({ data, onMarkerClick, selectedPoint }) => {
   const [viewport, setViewport] = useState({
     latitude: 28.53,
     longitude: 77.22,
     zoom: 9
   });
-
   const markerImages = {
     Completed: "/free-map-marker-icon-green.png",
     WIP: "/free-map-marker-icon-orange.png",
     default: "/free-map-marker-icon-green.png",
   };
-
   const getMarkerImage = (status) => {
     return markerImages[status] || markerImages.default;
   };
-
   return (
     <ReactMapGL
       {...viewport}
       width="100%"
       height="100%"
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      mapboxApiAccessToken="pk.eyJ1IjoibmlraGlsc2FyYWYiLCJhIjoiY2xlc296YjRjMDA5dDNzcXphZjlzamFmeSJ9.7ZDaMZKecY3-70p9pX9-GQ" // Make sure this is your correct Mapbox access token
+      onViewportChange={setViewport}
+      mapboxApiAccessToken="your-mapbox-access-token"
+      mapboxApiAccessToken="pk.eyJ1IjoibmlraGlsc2FyYWYiLCJhIjoiY2xlc296YjRjMDA5dDNzcXphZjlzamFmeSJ9.7ZDaMZKecY3-70p9pX9-GQ"
       mapStyle="mapbox://styles/mapbox/streets-v11"
     >
       {data.map((point) => (
@@ -43,7 +40,6 @@ const Map = ({ data, onMarkerClick, selectedPoint }) => {
               transition: 'transform 0.3s ease-out'
             }}
             onClick={(e) => {
-              e.preventDefault();  // Prevent default to avoid any page navigation or other unwanted actions
               e.stopPropagation();  // Stop the event from bubbling up to the map
               onMarkerClick(point);
             }}
@@ -59,5 +55,4 @@ const Map = ({ data, onMarkerClick, selectedPoint }) => {
     </ReactMapGL>
   );
 };
-
 export default Map;

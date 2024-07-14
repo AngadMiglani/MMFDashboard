439a4c28e802d41918fbd8e0aee1c96fef1f917a
AngadMiglani <82772584+AngadMiglani@users.noreply.github.com>
2024-07-14 23:24:43 +0530

    Undo change

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,6 @@ const App = () => {
             <option value="All">All</option>
             <option value="Completed">Completed</option>
             <option value="WIP">Work in Progress</option>
-            <option value="Approved">Approved by MCD</option>
           </select>
         </div>
         <Map data={filteredData} />

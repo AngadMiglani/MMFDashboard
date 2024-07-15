7ca305ac542cb3f6952515beba63db8de97d2617
AngadMiglani <82772584+AngadMiglani@users.noreply.github.com>
2024-07-15 16:31:55 +0530

    green darker update

--- src/Map.tsx
+++ src/Map.tsx
@@ -7,7 +7,7 @@ const Map = ({ data }) => {
   const [showDetails, setShowDetails] = useState(false);
 
   const markerImages = {
-    Completed: "/free-map-marker-icon-green.png", // Updated icon
+    Completed: "/free-map-marker-icon-green-darker.png", // Updated icon
     WIP: "/free-map-marker-icon-pink.png", // Updated icon
     Approved: "/free-map-marker-icon-blue-darker.png", // New category icon
     default: "/free-map-marker-icon-green-darker.png", // Updated default icon

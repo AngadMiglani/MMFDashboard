b049343784b4b45f1def8fbef5683271fdb644f5
AngadMiglani <82772584+AngadMiglani@users.noreply.github.com>
2024-07-29 08:05:39 +0530

    Still debugging

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       <Helmet>
-        <title>Million Miyawaki Foundation</title>
+        <title>Million Miyawaki Dashboard</title>
       </Helmet>
       <div className="statistics-section">
         <div className="stat-box">

1322710cbcc19d43e1f314be07f34939e7f13e14
AngadMiglani <82772584+AngadMiglani@users.noreply.github.com>
2025-02-18 09:23:48 +0530

    school id

--- src/api.ts
+++ src/api.ts
@@ -6,22 +6,23 @@ export const fetchDataFromSheet = async () => {
       )
     ).json();
 
-    return response.map(({ name, latitude, longitude, status, area, numsaplings, plantationdate, lastinspectiondate, image, schoolId ...props }) => ({
-      name: name || "Unknown", // Provide a default value if name is not present
-      latitude: latitude ? parseFloat(latitude) : 0, // Default to 0 if latitude is not present
-      longitude: longitude ? parseFloat(longitude) : 0, // Default to 0 if longitude is not present
-      status: status || "Unknown", // Provide a default value if status is not present
-      area: area ? parseInt(area) : 0, // Default to 0 if area is not present
-      numsaplings: numsaplings ? parseInt(numsaplings) : 0, // Default to 0 if numsaplings is not present
-      plantationdate: plantationdate || "Unknown", // Provide a default value if plantationdate is not present
-      lastinspectiondate: lastinspectiondate || "Unknown", // Provide a default value if lastinspectiondate is not present
-      image: image || "", // Base URL for images (folder URL)
-      schoolId: schoolId || "", // School ID to be used in constructing the image folder path
-      ...props,
-    }));
+    return response.map(
+      ({ name, latitude, longitude, status, area, numsaplings, plantationdate, lastinspectiondate, image, schoolId, ...props }) => ({
+        name: name || "Unknown", // Provide a default value if name is not present
+        latitude: latitude ? parseFloat(latitude) : 0, // Default to 0 if latitude is not present
+        longitude: longitude ? parseFloat(longitude) : 0, // Default to 0 if longitude is not present
+        status: status || "Unknown", // Provide a default value if status is not present
+        area: area ? parseInt(area) : 0, // Default to 0 if area is not present
+        numsaplings: numsaplings ? parseInt(numsaplings) : 0, // Default to 0 if numsaplings is not present
+        plantationdate: plantationdate || "Unknown", // Provide a default value if plantation date is not present
+        lastinspectiondate: lastinspectiondate || "Unknown", // Provide a default value if last inspection date is not present
+        image: image || "", // Base URL for images (folder URL)
+        schoolId: schoolId || "", // School ID to be used in constructing the image folder path
+        ...props,
+      })
+    );
   } catch (error) {
     console.error("Error fetching data: ", error);
     return [];
   }
 };
-
